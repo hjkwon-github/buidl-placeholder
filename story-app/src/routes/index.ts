422456729698cb189078ed6b2ca3ca27a73bfb23
ddevkim <ddevkim@gmail.com>
2025-04-12 14:56:11 +0900

    feat: add IP routing to the API for improved functionality

--- story-app/src/routes/index.ts
+++ story-app/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { storyRouter } from './story.routes';
+import { ipRouter } from './ip.routes';
 
 const router = Router();
 
+// 라우트 등록
 router.use('/stories', storyRouter);
+router.use('/v1/ip', ipRouter);
 
-export { router as routes }; 
+export { router as routes };
